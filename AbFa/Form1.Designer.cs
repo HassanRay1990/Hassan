981a811251035126575965686dac2668886cc062
Hassan Rahgoshay <rahgoshay90@hotmail.com>
2020-02-02 01:42:09 +0330

    First Commit

---
+++ AbFa/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace AbFa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLitr = new System.Windows.Forms.TextBox();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.txtVahed = new System.Windows.Forms.TextBox();
+            this.txtZshahr = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtAbBaha = new System.Windows.Forms.TextBox();
+            this.txtAbonman = new System.Windows.Forms.TextBox();
+            this.txtFazelab = new System.Windows.Forms.TextBox();
+            this.txtAbonmanF = new System.Windows.Forms.TextBox();
+            this.txtMaliat = new System.Windows.Forms.TextBox();
+            this.txtTabsare = new System.Windows.Forms.TextBox();
+            this.txtKol = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "مصرف به لیتر";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "مدت قرائت";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "واحد مسکونی";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ضریب شهر";
+            // 
+            // txtLitr
+            // 
+            this.txtLitr.Location = new System.Drawing.Point(289, 21);
+            this.txtLitr.Name = "txtLitr";
+            this.txtLitr.Size = new System.Drawing.Size(100, 20);
+            this.txtLitr.TabIndex = 5;
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(289, 48);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(100, 20);
+            this.txtDays.TabIndex = 6;
+            // 
+            // txtVahed
+            // 
+            this.txtVahed.Location = new System.Drawing.Point(289, 74);
+            this.txtVahed.Name = "txtVahed";
+            this.txtVahed.Size = new System.Drawing.Size(100, 20);
+            this.txtVahed.TabIndex = 8;
+            // 
+            // txtZshahr
+            // 
+            this.txtZshahr.Location = new System.Drawing.Point(289, 100);
+            this.txtZshahr.Name = "txtZshahr";
+            this.txtZshahr.Size = new System.Drawing.Size(100, 20);
+            this.txtZshahr.TabIndex = 9;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(289, 142);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 38);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "حساب کن";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "آب بهاء";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(137, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "آبونمان آب";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "خدمات دفع فاضلاب";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(137, 103);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "آبونمان فاضلاب";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(137, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "مالیات بر ارزش افزوده";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(137, 163);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "بند ه تبصره 9 بودجه";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(137, 198);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(43, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "جمع کل";
+            // 
+            // txtAbBaha
+            // 
+            this.txtAbBaha.Location = new System.Drawing.Point(12, 21);
+            this.txtAbBaha.Name = "txtAbBaha";
+            this.txtAbBaha.ReadOnly = true;
+            this.txtAbBaha.Size = new System.Drawing.Size(100, 20);
+            this.txtAbBaha.TabIndex = 18;
+            // 
+            // txtAbonman
+            // 
+            this.txtAbonman.Location = new System.Drawing.Point(12, 48);
+            this.txtAbonman.Name = "txtAbonman";
+            this.txtAbonman.ReadOnly = true;
+            this.txtAbonman.Size = new System.Drawing.Size(100, 20);
+            this.txtAbonman.TabIndex = 19;
+            // 
+            // txtFazelab
+            // 
+            this.txtFazelab.Location = new System.Drawing.Point(12, 74);
+            this.txtFazelab.Name = "txtFazelab";
+            this.txtFazelab.ReadOnly = true;
+            this.txtFazelab.Size = new System.Drawing.Size(100, 20);
+            this.txtFazelab.TabIndex = 20;
+            // 
+            // txtAbonmanF
+            // 
+            this.txtAbonmanF.Location = new System.Drawing.Point(12, 100);
+            this.txtAbonmanF.Name = "txtAbonmanF";
+            this.txtAbonmanF.ReadOnly = true;
+            this.txtAbonmanF.Size = new System.Drawing.Size(100, 20);
+            this.txtAbonmanF.TabIndex = 21;
+            // 
+            // txtMaliat
+            // 
+            this.txtMaliat.Location = new System.Drawing.Point(12, 128);
+            this.txtMaliat.Name = "txtMaliat";
+            this.txtMaliat.ReadOnly = true;
+            this.txtMaliat.Size = new System.Drawing.Size(100, 20);
+            this.txtMaliat.TabIndex = 22;
+            // 
+            // txtTabsare
+            // 
+            this.txtTabsare.Location = new System.Drawing.Point(12, 160);
+            this.txtTabsare.Name = "txtTabsare";
+            this.txtTabsare.ReadOnly = true;
+            this.txtTabsare.Size = new System.Drawing.Size(100, 20);
+            this.txtTabsare.TabIndex = 23;
+            // 
+            // txtKol
+            // 
+            this.txtKol.Location = new System.Drawing.Point(12, 191);
+            this.txtKol.Name = "txtKol";
+            this.txtKol.ReadOnly = true;
+            this.txtKol.Size = new System.Drawing.Size(100, 20);
+            this.txtKol.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 233);
+            this.Controls.Add(this.txtKol);
+            this.Controls.Add(this.txtTabsare);
+            this.Controls.Add(this.txtMaliat);
+            this.Controls.Add(this.txtAbonmanF);
+            this.Controls.Add(this.txtFazelab);
+            this.Controls.Add(this.txtAbonman);
+            this.Controls.Add(this.txtAbBaha);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtZshahr);
+            this.Controls.Add(this.txtVahed);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.txtLitr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLitr;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.TextBox txtVahed;
+        private System.Windows.Forms.TextBox txtZshahr;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtAbBaha;
+        private System.Windows.Forms.TextBox txtAbonman;
+        private System.Windows.Forms.TextBox txtFazelab;
+        private System.Windows.Forms.TextBox txtAbonmanF;
+        private System.Windows.Forms.TextBox txtMaliat;
+        private System.Windows.Forms.TextBox txtTabsare;
+        private System.Windows.Forms.TextBox txtKol;
+    }
+}
+
---
+++ AbFa/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbFa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double MonthLA;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double cubeMeter, days, CityCoe, Units, MonthL, AbBaha, Abonman, KhDFazelab, Maliat, Tabsare, kol;
+
+            cubeMeter = Convert.ToDouble(txtLitr.Text) / 1000;
+            days = Convert.ToDouble(txtDays.Text);
+            CityCoe = Convert.ToDouble(txtZshahr.Text);
+            Units = Convert.ToDouble(txtVahed.Text);
+
+            MonthL = cubeMeter * 30 / days / Units;
+
+            if (MonthL > 0 && MonthL <= 5)
+            {
+                MonthLA = 1419 * MonthL;
+            }
+            else if (MonthL > 5 && MonthL <= 10)
+            {
+                MonthLA = 2123 * MonthL - 3520;
+            }
+            else if (MonthL > 10 && MonthL <= 15)
+            {
+                MonthLA = 2827 * MonthL - 10560;
+            }
+            else if (MonthL > 15 && MonthL <= 20)
+            {
+                MonthLA = 3703 * MonthL - 23700;
+            }
+            else if (MonthL > 20 && MonthL <= 25)
+            {
+                MonthLA = 5400 * MonthL - 57640;
+            }
+            else if (MonthL > 25 && MonthL <= 30)
+            {
+                MonthLA = 8496 * MonthL - 135040;
+            }
+            else if (MonthL > 30 && MonthL <= 35)
+            {
+                MonthLA = 11580 * MonthL - 227560;
+            }
+            else if (MonthL > 35 && MonthL <= 40)
+            {
+                MonthLA = 15444 * MonthL - 362800;
+            }
+            else if (MonthL > 40 && MonthL <= 50)
+            {
+                MonthLA = 33462 * MonthL - 1083520;
+            }
+            else if (MonthL > 50)
+            {
+                MonthLA = 66924 * MonthL - 2756620;
+            }
+
+            AbBaha = MonthLA * Units * days * CityCoe / 30;
+            txtAbBaha.Text = Math.Round(AbBaha).ToString();
+
+            Abonman = days * Units * 10000 / 30;
+            txtAbonman.Text = Math.Round(Abonman).ToString();
+            txtAbonmanF.Text = Math.Round(Abonman).ToString();
+
+            KhDFazelab = AbBaha * 0.7;
+            txtFazelab.Text = Math.Round(KhDFazelab).ToString();
+
+            Maliat = (AbBaha + Abonman * 2 + KhDFazelab) * 0.09;
+            txtMaliat.Text = Math.Round(Maliat).ToString();
+
+            Tabsare = cubeMeter * 150;
+            txtTabsare.Text = Math.Round(Tabsare).ToString();
+
+            kol = AbBaha + Abonman * 2 + KhDFazelab + Maliat + Tabsare;
+            txtKol.Text = Math.Round(kol).ToString();
+
+        }
+    }
+}
